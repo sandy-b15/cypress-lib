efc67b7dabf7f04bb89ae155069c0ff8418af990
Siva0703 <58432968+Siva0703@users.noreply.github.com>
2024-08-27 18:52:53 +0530

    misc

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   defaultCommandTimeout: 10000,
@@ -28,9 +28,10 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts')(on, config)
+      // eslint-disable-next-line import/extensions
+      return require('./cypress/plugins/index.ts')(on, config);
     },
     baseUrl: 'https://github.com/',
     excludeSpecPattern: '*.js',
   },
-})
+});
