ff98219f21d0359f3e4eced6fdaae49c3f4bdc7e
Sandy <baligerisandeep@gmail.com>
2024-08-27 09:59:46 +0530

    first commit

---
+++ cypress.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  defaultCommandTimeout: 10000,
+  requestTimeout: 10000,
+  responseTimeout: 10000,
+  pageLoadTimeout: 10000,
+  video: true,
+  chromeWebSecurity: true,
+  watchForFileChanges: false,
+  failOnStatusCode: false,
+  viewportWidth: 1440,
+  viewportHeight: 900,
+  screenshotOnRunFailure: true,
+  restartBrowserBetweenSpecFiles: true,
+  reporter: 'sandy',
+  reporterOptions: {
+    reportDir: 'cypress/reports/integration',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.ts')(on, config)
+    },
+    baseUrl: 'https://github.com/',
+    excludeSpecPattern: '*.js',
+  },
+})
